f70129db9711f5578b0d42f574fcf4bfefc111ec
Escarribur <escarribur2@gmail.com>
2016-11-30 21:33:02 -0300

    firt push lucho

--- backend/src/main/java/ProductoTest.java
+++ backend/src/main/java/ProductoTest.java
@@ -6,7 +6,7 @@ public class ProductoTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		fail("probando el jenkins pls");
 	}
 
 }
---
+++ frontend/mantis/config/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'mantisbt_db';
+$g_db_username            = 'mantis';
+$g_db_password            = 'tingeso123';
+
+$g_default_timezone       = 'America/Santiago';
+
+$g_crypto_master_salt     = '79my7moS7MOE18Ryx1b7XMNndWsPJIowpj+l+v4R6kE=';
+
